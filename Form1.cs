8fa8ad81117f80f44d324bead370151fa6443d95
annastepankova <160622621+annastepankova@users.noreply.github.com>
2024-12-06 00:07:20 +0700

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-namespace курсовая_работа_3_курс__салон_красоты_
+namespace курсовая_работа_3_курс__салон_красоты_//Название пространства имен слишком длинное и неудобное. Использование подчеркиваний (`_`) также не рекомендуется в соответствии с рекомендациями по написанию кода C#.
 {
     public partial class Form1 : Form
     {
@@ -21,11 +21,11 @@ namespace курсовая_работа_3_курс__салон_красоты_
             authorizationForm.ShowDialog();
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void button3_Click(object sender, EventArgs e)//В методе `button3_Click` используется та же форма, что и для администратора (`Form3`). Это неправильно, если функционал для клиента отличается.
         {
             //переъод на форму клиента
             Form3 klientForm = new Form3();
-            klientForm.ShowDialog();
+            klientForm.ShowDialog();//ShowDialog()` блокирует основную форму до тех пор, пока модальная форма не будет закрыта. Это может быть неудобно для пользователя.
         }
     }
 }
